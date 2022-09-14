12fc956f81e6a819e9f241cb8bd2b8ef7c3853ab
Mohmed Atef <88455397+Mohamedate@users.noreply.github.com>
2022-09-14 17:58:15 +0200

    Update java.js

--- java.js
+++ java.js
@@ -1,23 +1,33 @@
 let btn = document.querySelector(".btn-one");
-let se = document.querySelector(".two");
-console.log(btn);
+let seTwo = document.querySelector(".two");
+
 function shwoSection() { 
-se.classList.add("shwo");
+seTwo.classList.add("shwo");
 }
 
 
 
 let user = document.querySelector("[name='user']");
 let age = document.querySelector("[name='age']");
-let seThree = document.querySelector(".three");
-console.log(age)
-console.log(user)
+let seThree = document.querySelector(".three"),
+happyName = document.querySelector(".three .name"),
+happyAge = document.querySelector(".three .age"),
+box = document.querySelector(".box");
+
 
 document.forms[0].onsubmit = function (e) { 
 let userValid = false;
 let ageValid = false;
- if (user.value === "shosho" && age.value === "19") { 
+ if (/[a-z]{3,}/gi.test(user.value) && /\d{2}/gi.test(age.value)) { 
+    happyName.innerHTML = user.value;
+    happyAge.innerHTML = `Sweet ${age.value}`;
      seThree.classList.add("shwo");
+
+     setInterval(function() { 
+       box.classList.add("effect");
+       
+     },2500)
+     document.getElementById("happySong").play();
 
  }  else { 
     document.querySelector(".error").classList.toggle("run");
